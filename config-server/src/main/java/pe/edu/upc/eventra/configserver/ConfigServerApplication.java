703dcf7e0404ca289bbb4bc46262663ad1a0c260
Jack Arana <arack.dev@gmail.com>
2024-05-10 01:23:34 -0500

    feat(microservice): add config-server module

---
+++ config-server/src/main/java/pe/edu/upc/eventra/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.eventra.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
---
+++ config-server/src/test/java/pe/edu/upc/eventra/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.eventra.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
