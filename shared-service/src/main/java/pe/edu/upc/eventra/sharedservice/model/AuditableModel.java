57b6e6c90d8f37fc99dccd290f598b09f700a57e
Jack Arana <arack.dev@gmail.com>
2024-05-11 01:44:22 -0500

    feat(model): add AuditableModel.java

---
+++ shared-service/src/main/java/pe/edu/upc/eventra/sharedservice/model/AuditableModel.java
@@ -0,0 +1,27 @@
+package pe.edu.upc.eventra.sharedservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class AuditableModel {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Date updatedAt;
+}
