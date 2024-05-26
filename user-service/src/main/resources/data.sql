0f205f83f3c2e7ce116df40e536db024e59d2d96
EEduardoSanandres <102604575+EEduardoSanandres@users.noreply.github.com>
2024-05-25 23:11:10 -0500

    Actualizaciones

--- user-service/src/main/resources/data.sql
+++ user-service/src/main/resources/data.sql
@@ -3,5 +3,5 @@ INSERT INTO typeofuser (description) VALUES ('Admin');
 INSERT INTO typeofuser (description) VALUES ('User');
 
 -- Insertar datos iniciales en users
-INSERT INTO users (first_name, last_name, email, password, type_id) VALUES ('John', 'Doe', 'john@example.com', 'password', 1);
-INSERT INTO users (first_name, last_name, email, password, type_id) VALUES ('Jane', 'Doe', 'jane@example.com', 'password', 2);
+INSERT IGNORE INTO users (first_name, last_name, email, password, type_id) VALUES ('John', 'Doe', 'john@example.com', 'password', 1);
+INSERT IGNORE INTO users (first_name, last_name, email, password, type_id) VALUES ('Jane', 'Doe', 'jane@example.com', 'password', 2);
