1cba6ba16b140bc971033cb875cb388af405a8d4
EEduardoSanandres <102604575+EEduardoSanandres@users.noreply.github.com>
2024-06-23 00:56:01 -0500

    Upgrade

--- tickets-service/src/main/resources/data.sql
+++ tickets-service/src/main/resources/data.sql
@@ -1,6 +1,12 @@
--- Insertar datos iniciales en tickets
-INSERT IGNORE INTO tickets (eventID, price, total_available, category, description)
-VALUES
-    (1, 50.0, 100, 'Concert', 'Description of concert ticket'),
-    (2, 25.0, 50, 'Theater', 'Description of theater ticket'),
-    (3, 20.0, 200, 'Sports', 'Description of sports ticket');
+-- Insertar datos iniciales en tickets si no existen
+INSERT INTO tickets (eventID, price, total_available, category, description)
+SELECT 1, 50.0, 100, 'Concert', 'Description of concert ticket'
+    WHERE NOT EXISTS (SELECT 1 FROM tickets WHERE eventID = 1);
+
+INSERT INTO tickets (eventID, price, total_available, category, description)
+SELECT 2, 25.0, 50, 'Theater', 'Description of theater ticket'
+    WHERE NOT EXISTS (SELECT 1 FROM tickets WHERE eventID = 2);
+
+INSERT INTO tickets (eventID, price, total_available, category, description)
+SELECT 3, 20.0, 200, 'Sports', 'Description of sports ticket'
+    WHERE NOT EXISTS (SELECT 1 FROM tickets WHERE eventID = 3);
